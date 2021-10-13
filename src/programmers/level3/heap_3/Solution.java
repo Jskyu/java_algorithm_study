8c13930de4e901dc6413fb2703cc041027ee3646
Jskyu <hisunggyu@gmail.com>
2021-10-13 18:00:49 +0900

    리팩토링

--- src/programmers/level3/heap_3/Solution.java
+++ src/programmers/level3/heap_3/Solution.java
@@ -1,5 +1,6 @@
 package programmers.level3.heap_3;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -7,8 +8,9 @@ class Solution {
     public int[] solution(String[] operations) {
         Queue<Integer> maxQ = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
 
-        for (String str : operations) {
-            String order = str.split(" ")[0];
+        Arrays.stream(operations).forEach(str -> {
+            String[] split = str.split(" ");
+            String order = split[0];
             int num = Integer.parseInt(str.split(" ")[1]);
 
             switch (order) {
@@ -16,22 +18,21 @@ class Solution {
                     maxQ.add(num);
                     break;
                 case "D":
-                    if(maxQ.isEmpty()) continue;
-                    else if (num == 1) {
-                        maxQ.remove();
-                    } else if (num == -1) {
-                        Queue<Integer> minQ = new PriorityQueue<>();
-
-                        minQ.addAll(maxQ);
-                        minQ.remove();
-
-                        maxQ.clear();
-                        maxQ.addAll(minQ);
+                    if (!maxQ.isEmpty()) {
+                        if (num == 1) {
+                            maxQ.remove();
+                        } else if (num == -1) {
+                            removeMin(maxQ);
+                        }
+                        break;
                     }
-                    break;
             }
-        }
+        });
 
+        return getResult(maxQ);
+    }
+
+    private int[] getResult(Queue<Integer> maxQ) {
         if (maxQ.isEmpty()) return new int[]{0, 0};
         int max = maxQ.peek();
         int min = maxQ.poll();
@@ -39,5 +40,15 @@ class Solution {
             min = maxQ.poll();
         }
         return new int[]{max, min};
+    }
+
+    private void removeMin(Queue<Integer> maxQ) {
+        Queue<Integer> minQ = new PriorityQueue<>();
+
+        minQ.addAll(maxQ);
+        minQ.remove();
+
+        maxQ.clear();
+        maxQ.addAll(minQ);
     }
 }
