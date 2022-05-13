4258e862785b18753040b648a2c3e14580f65e56
Jskyu <hisunggyu@gmail.com>
2022-05-13 14:56:22 +0900

    프로그래머스 Level2 타겟 넘버 https://programmers.co.kr/learn/courses/30/lessons/43165

---
+++ src/programmers/level2/targetNumber/Solution.java
@@ -0,0 +1,23 @@
+package programmers.level2.targetNumber;
+
+public class Solution {
+    int TARGET = 0;
+    int[] NUMBERS = new int[0];
+
+    public int solution(int[] numbers, int target) {
+        TARGET = target;
+        NUMBERS = numbers;
+        return getTargetCount(0, 0);
+    }
+
+    private int getTargetCount(int number, int index) {
+        if (index == NUMBERS.length) {
+            if (number == TARGET) {
+                return 1;
+            }
+            return 0;
+        }
+        return getTargetCount(number + NUMBERS[index], index + 1) +
+                getTargetCount(number - NUMBERS[index], index + 1);
+    }
+}
