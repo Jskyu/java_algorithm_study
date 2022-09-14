291d018c84dc7337598dfee126ddb5e60dc910fe
Jskyu <hisunggyu@gmail.com>
2022-09-14 13:26:12 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/12941 최솟값 만들기

---
+++ src/programmers/level2/최솟값_만들기/Solution.java
@@ -0,0 +1,18 @@
+package programmers.level2.최솟값_만들기;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < A.length; i++) {
+            answer += (A[i] * B[A.length - 1 - i]);
+        }
+
+        return answer;
+    }
+}
