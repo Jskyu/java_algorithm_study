29df84cc8fbc5eded16edba1d1d5317f3e1a388e
jskyu <hisunggyu@gmail.com>
2023-12-05 14:52:15 +0900

    PCCP NUM1. 붕대감기 https://school.programmers.co.kr/learn/courses/30/lessons/250137

---
+++ src/programmers/pccp/num1_붕대감기/Solution.java
@@ -0,0 +1,35 @@
+package programmers.pccp.num1_붕대감기;
+
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int healTime = bandage[0];
+        int healPerSec = bandage[1];
+        int healBonus = bandage[2];
+        int max = health;
+
+        int time = 0;
+        for (int i = 0; i < attacks.length; i++) {
+            int healCount = 0;
+            int attackTime = attacks[i][0];
+            int attackDamage = attacks[i][1];
+
+            for (int j = time; j < attackTime; j++) {
+                health += healPerSec;
+                if (++healCount >= healTime) {
+                    healCount = 0;
+                    health += healBonus;
+                }
+                if (health > max) {
+                    health = max;
+                }
+                time++;
+            }
+            time++;
+            health -= attackDamage;
+
+            if (health <= 0) return -1;
+        }
+
+        return health;
+    }
+}
