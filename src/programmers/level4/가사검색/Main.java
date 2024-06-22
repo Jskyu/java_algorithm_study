c6ebf21527b984108214ef61a944f34ff12d1730
jsk <hisunggyu@gmail.com>
2024-06-23 02:35:45 +0900

    LV4. 가사 검색 조건 오류수정 https://programmers.co.kr/learn/courses/30/lessons/60060

--- src/programmers/level4/가사검색/Main.java
+++ src/programmers/level4/가사검색/Main.java
@@ -1,8 +1,10 @@
 package programmers.level4.가사검색;
 
-//https://programmers.co.kr/learn/courses/30/lessons/60060
-/**
- 가사 검색
+//
+
+import java.util.Arrays;
+
+/**<a href="https://programmers.co.kr/learn/courses/30/lessons/60060">가사 검색</a>
 
  친구들로부터 천재 프로그래머로 불리는 "프로도"는 음악을 하는 친구로부터 자신이 좋아하는 노래 가사에 사용된 단어들 중에
  특정 키워드가 몇 개 포함되어 있는지 궁금하니 프로그램으로 개발해 달라는 제안을 받았습니다.
@@ -19,6 +21,7 @@ package programmers.level4.가사검색;
  전체 가사 단어 길이의 합은 2 이상 1,000,000 이하입니다.
  가사에 동일 단어가 여러 번 나올 경우 중복을 제거하고 words에는 하나로만 제공됩니다.
  각 가사 단어는 오직 알파벳 소문자로만 구성되어 있으며, 특수문자나 숫자는 포함하지 않는 것으로 가정합니다.
+
  검색 키워드 제한사항
  queries의 길이(검색 키워드 개수)는 2 이상 100,000 이하입니다.
  각 검색 키워드의 길이는 1 이상 10,000 이하로 빈 문자열인 경우는 없습니다.
@@ -28,6 +31,7 @@ package programmers.level4.가사검색;
  검색 키워드는 와일드카드 문자인 '?'가 하나 이상 포함돼 있으며, '?'는 각 검색 키워드의 접두사 아니면 접미사 중 하나로만 주어집니다.
  예를 들어 "??odo", "fro??", "?????"는 가능한 키워드입니다.
  반면에 "frodo"('?'가 없음), "fr?do"('?'가 중간에 있음), "?ro??"('?'가 양쪽에 있음)는 불가능한 키워드입니다.
+
  입출력 예
  words	                                                    queries                                     	result
  ["frodo", "front", "frost", "frozen", "frame", "kakao"]	["fro??", "????o", "fr???", "fro???", "pro?"]	[3, 2, 4, 1, 0]
@@ -42,13 +46,11 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
-        String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?", "fr???", "?????", "", "?????", "??????", "?"};
+        String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?", "fr???", "?????", "??????", "?"};
+        int[] answers = {3, 2, 4, 1, 0, 4, 5, 1, 0};
 
         int[] result = s.solution(words, queries);
-        System.out.print("result = ");
-        for (int i : result) {
-            System.out.print(i + " ");
-        }
-
+        System.out.println("results = " + Arrays.toString(result));
+        System.out.println("answers = " + Arrays.toString(answers));
     }
 }
--- src/programmers/level4/가사검색/Solution.java
+++ src/programmers/level4/가사검색/Solution.java
@@ -9,12 +9,12 @@ class Solution {
         long start = System.currentTimeMillis();
         try {
             int[] answer = new int[queries.length];
-            Trie trie = new FrontTrie();
-            Trie sufTrie = new BackTrie();
+            Trie frontTrie = new FrontTrie();
+            Trie suffixTrie = new BackTrie();
             Map<String, Integer> resultMap = new HashMap<>();
 
-            Arrays.stream(words).forEach(trie::insert);
-            Arrays.stream(words).forEach(sufTrie::insert);
+            Arrays.stream(words).forEach(frontTrie::insert);
+            Arrays.stream(words).forEach(suffixTrie::insert);
 
             Arrays.stream(queries)
                     .distinct() //중복제거
@@ -22,9 +22,9 @@ class Solution {
                 if (query.matches("[?]+")) {// ?로만 구성된 검색키워드
                     resultMap.put(query, (int) Arrays.stream(words).filter(str -> str.length() == query.length()).count());
                 } else if (query.startsWith("?")) {// ?로 시작하는 검색 키워드
-                    resultMap.put(query, sufTrie.getCount(query.substring(query.lastIndexOf("?") + 1), query.length()));
+                    resultMap.put(query, suffixTrie.getCount(query.substring(query.lastIndexOf("?") + 1), query.length()));
                 } else {// ?로 끝나는 검색 키워드
-                    resultMap.put(query, trie.getCount(query.substring(0, query.indexOf("?")), query.length()));
+                    resultMap.put(query, frontTrie.getCount(query.substring(0, query.indexOf("?")), query.length()));
                 }
             });
 
@@ -44,7 +44,18 @@ class Solution {
 
         int getCount(String word, int len);
 
-        int getChildNodeCount(Node node, int len);
+        default int getChildNodeCount(Node node, int len){
+            int count = 0;
+            if (len <= 0) {
+                if (node.isEndOfWord()) return 1;
+                return 0;
+            } else {
+                for (Node childNode : node.getChildes().values()) {
+                    count += getChildNodeCount(childNode, len - 1);
+                }
+            }
+            return count;
+        }
     }
 
     static class FrontTrie implements Trie {
@@ -73,20 +84,6 @@ class Solution {
 
             return getChildNodeCount(node, len - word.length());
         }
-
-        @Override
-        public int getChildNodeCount(Node node, int len) {
-            int count = 0;
-            if (len <= 0) {
-                if (node.isEndOfWord()) return 1;
-                return 0;
-            } else {
-                for (Node childNode : node.getChildes().values()) {
-                    count += getChildNodeCount(childNode, len - 1);
-                }
-            }
-            return count;
-        }
     }
 
     static class BackTrie implements Trie {
@@ -114,20 +111,6 @@ class Solution {
             }
 
             return getChildNodeCount(node, len - word.length());
-        }
-
-        @Override
-        public int getChildNodeCount(Node node, int len) {
-            int count = 0;
-            if (len <= 0) {
-                if (node.isEndOfWord()) return 1;
-                return 0;
-            } else {
-                for (Node childNode : node.getChildes().values()) {
-                    count += getChildNodeCount(childNode, len - 1);
-                }
-            }
-            return count;
         }
     }
 
